2efdc7b93eb04f654466ec903d1b79dd90297f90
Jesdhy <pinchajessy529@gmail.com>
2025-06-10 23:20:34 -0500

    refactor: :recycle: Update file
    Update test

--- test_app.py
+++ test_app.py
@@ -1,4 +1,14 @@
 from app import app
 
-def test_example():
-    return "Hello World test"
+def test_home_route():
+    # customer
+    client = app.test_client()
+    
+    #create GET a "/"
+    response = client.get("/")
+    
+    # send respose for test 200 OK
+    assert response.status_code == 200
+    
+    # check menssage
+    assert b"Hello World" in response.data
